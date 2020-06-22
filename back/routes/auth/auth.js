0a807b63b567a4f2a9671cedc2721bc467485cc6
jeanlouis <59745606+jeanlouis6113@users.noreply.github.com>
2020-06-22 17:14:30 +0200

    quete 040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const connection = require('../../helpers/db.js');
+
+router.get("/signup", function (req, res, next) {
+    connection.query('SELECT * from users', (err, results) => {
+
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération du contenu');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
 
 router.post("/signup", function (req, res, next) {
-    res.send("I am in POST signup");
-  });
+    const formData = req.body;
+    connection.query("INSERT INTO users SET ?", formData, (err, results) => {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
 
 
 
