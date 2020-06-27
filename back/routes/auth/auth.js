a9631ab4bd3e28264495c95ab4ce25bde7c9b554
jeanlouis <59745606+jeanlouis6113@users.noreply.github.com>
2020-06-27 15:38:29 +0200

    quete 070

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -3,18 +3,14 @@ const connection = require("../../helpers/db");
 const router = express.Router();
 
 router.post("/signup", function (req, res, next) {
-    const formData = req.body;
-    connection.query("INSERT INTO users SET ?", formData, (err, results) => {
-        if (err) {
-            res.status(500).send({ flash: err.message });
-        } else {
-            res.status(200).send({ flash: "User has been signed up !" });
-        }
-    });
+  const formData = req.body;
+  connection.query("INSERT INTO users SET ?", formData, (err, results) => {
+    if (err) {
+      res.status(500).send({flash: err.message});
+    } else {
+      res.status(200).send({flash: "User has been signed up !"});
+    }
+  });
 });
 
-
-
 module.exports = router;
-
-
